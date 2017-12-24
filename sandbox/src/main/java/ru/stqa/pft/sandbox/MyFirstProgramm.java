96cffdf0f33156b0800b236146f29991de650cf0
Turist400 <turist400@gmail.com>
2017-12-24 17:52:49 +0300

    Created package ru.stqa.pft.sandbox

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgramm {
+  public static void main(String[] args) {
+    String h = "Hello";
+    String w = "world";
+    int l = 5;
+    int s = l * l ;
+
+    System.out.println(h + " " + w + " " + s + " " + "!!!");
+
+
+  }
+
+}

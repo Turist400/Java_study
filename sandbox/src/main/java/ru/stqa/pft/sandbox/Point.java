a634f908e21ba8d057f0626dd38dac27873afd75
Mikhail Pushkarev <turist400@gmail.com>
2017-12-25 21:05:08 +0300

    4й пункт 2го задания.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,6 +4,16 @@ public class Point {
 
   public double x;
   public double y;
+  public Point (){
+  this.x=x;
+  this.y=y;
+}
+
+  public double distance(Point p) {
+    double dx = this.x - p.x;
+    double dy = this.y - p.y;
+    return Math.sqrt(dx*dx+dy*dy);
+  }
 
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointProgramm.java
@@ -11,13 +11,10 @@ public class PointProgramm {
     p2.x = 2.8;
     p2.y = 2.2;
 
-    double dx = p1.x - p2.x;
-    double dy = p1.y - p2.y;
-    System.out.println("distance between points " + dx + " и " + dy + " = " + distance(dx, dy));
+
+    System.out.println("distance between points " + p1 + " и " + p2 + " = " + p1.distance(p2));
 
   }
 
-  public static double distance(double p1, double p2) {
-    return Math.sqrt(p1*p1 + p2*p2);
-  }
+
 }

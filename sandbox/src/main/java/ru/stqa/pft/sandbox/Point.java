4abec31aa4860526c233394f3a93c51d1b84dd8a
Mikhail Pushkarev <turist400@gmail.com>
2017-12-26 10:38:06 +0300

    Убрал лишние строки.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,18 +2,20 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-  public double x;
-  public double y;
-  public Point (){
-  this.x=x;
-  this.y=y;
-}
+    public double x;
+    public double y;
 
-  public double distance(Point p) {
-    double dx = this.x - p.x;
-    double dy = this.y - p.y;
-    return Math.sqrt(dx*dx+dy*dy);
-  }
+    public Point() {
+
+        //this.x = 0;
+        //this.y = 0;
+    }
+
+    public double distance(Point p) {
+        double dx = this.x - p.x;
+        double dy = this.y - p.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
 
 
 }

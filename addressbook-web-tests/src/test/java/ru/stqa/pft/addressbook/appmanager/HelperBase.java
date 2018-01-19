a25c4a3b5d9dbf28029b3e8f33447eb08ecddc55
Mikhail Pushkarev <turist400@gmail.com>
2018-01-19 18:56:06 +0300

    Lecture 3.5
    Add block 'if' for group creation.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -4,19 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HelperBase {
-  protected WebDriver wd;
+    protected WebDriver wd;
 
-  public HelperBase(WebDriver wd) {
-    this.wd = wd;
-  }
+    public HelperBase(WebDriver wd) {
+        this.wd = wd;
+    }
 
-  protected void click(By locator) {
-    wd.findElement(locator).click();
-  }
+    protected void click(By locator) {
+        wd.findElement(locator).click();
+    }
 
-  protected void type(By locator, String text) {
-    click(locator);
-    wd.findElement(locator).clear();
-    wd.findElement(locator).sendKeys(text);
-  }
+    protected void type(By locator, String text) {
+        click(locator);
+        if (text != null) {
+            wd.findElement(locator).clear();
+            wd.findElement(locator).sendKeys(text);
+        }
+    }
 }
